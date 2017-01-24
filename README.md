64d89ce74564950af3218d5888feae02c0edde63
Brad Scharmann <bscharm@gmail.com>
2017-01-23 21:37:06 -0700

    update README.md with correct working link

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Brainfuck Interpreter
-Working through the tutorial [here](https://thorstenball
-.com/blog/2017/01/04/a-virtual-brainfuck-machine-in-go) to practice TDD using Ginkgo and Gomega 
+Working through the tutorial [here](https://thorstenball.com/blog/2017/01/04/a-virtual-brainfuck-machine-in-go) to practice TDD using Ginkgo and Gomega 
 and better understand language interpretation.
 
  
--- machine_test.go
+++ machine_test.go
@@ -56,53 +56,53 @@ var _ = Describe("Machine", func() {
 			m.Execute()
 			Ω(m.memory[4]).Should(Equal(14))
 		})
-	})
 
-	It("handles the '-' symbol by decrementing the value in memory at dp by 1", func() {
-		m := Machine{}
-		m.dp = 6
-		m.memory[6] = 12
-		m.code = "-"
-		m.Execute()
-		Ω(m.memory[6]).Should(Equal(11))
-	})
+		It("handles the '-' symbol by decrementing the value in memory at dp by 1", func() {
+			m := Machine{}
+			m.dp = 6
+			m.memory[6] = 12
+			m.code = "-"
+			m.Execute()
+			Ω(m.memory[6]).Should(Equal(11))
+		})
 
-	It("handles the > symbol by increasing dp by 1", func() {
-		m := Machine{}
-		m.dp = 19
-		m.code = ">"
-		m.Execute()
-		Ω(m.dp).Should(Equal(20))
-	})
+		It("handles the > symbol by increasing dp by 1", func() {
+			m := Machine{}
+			m.dp = 19
+			m.code = ">"
+			m.Execute()
+			Ω(m.dp).Should(Equal(20))
+		})
 
-	It("handles the < symbol by decreasing dp by 1", func() {
-		m := Machine{}
-		m.dp = 11
-		m.code = "<"
-		m.Execute()
-		Ω(m.dp).Should(Equal(10))
-	})
+		It("handles the < symbol by decreasing dp by 1", func() {
+			m := Machine{}
+			m.dp = 11
+			m.code = "<"
+			m.Execute()
+			Ω(m.dp).Should(Equal(10))
+		})
 
-	It("handles the , symbol by calling readChar", func() {
-		reader := strings.NewReader(".")
-		m := Machine{}
-		m.input = reader
-		m.buf = make([]byte, 1)
-		m.dp = 4
-		m.code = ","
-		m.Execute()
-		Ω(m.memory[4]).Should(Equal(46))
-	})
+		It("handles the , symbol by calling readChar", func() {
+			reader := strings.NewReader(".")
+			m := Machine{}
+			m.input = reader
+			m.buf = make([]byte, 1)
+			m.dp = 4
+			m.code = ","
+			m.Execute()
+			Ω(m.memory[4]).Should(Equal(46))
+		})
 
-	It("handles the . symbol by calling putChar", func() {
-		var b bytes.Buffer
-		m := Machine{}
-		m.output = &b
-		m.buf = make([]byte, 1)
-		m.dp = 4
-		m.memory[4] = 60
-		m.code = "."
-		m.Execute()
-		Ω(b.String()).Should(Equal("<"))
+		It("handles the . symbol by calling putChar", func() {
+			var b bytes.Buffer
+			m := Machine{}
+			m.output = &b
+			m.buf = make([]byte, 1)
+			m.dp = 4
+			m.memory[4] = 60
+			m.code = "."
+			m.Execute()
+			Ω(b.String()).Should(Equal("<"))
+		})
 	})
 })
